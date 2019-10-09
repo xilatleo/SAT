e3665526a22c2b3643c801d4aca301736fae4f41
xilatleo <trinhhoangquan234@gmail.com>
2019-10-09 15:16:27 -0500

    Updated Contact Page. added Student Image folder

--- SAT.UI.MVC/Controllers/HomeController.cs
+++ SAT.UI.MVC/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using SAT.UI.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,11 +23,50 @@ namespace SAT.UI.MVC.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            
 
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(cvm);
+            }
+
+            string message = $"You have received an email from {cvm.Name} with a subject " + $"{cvm.Subject}. Please respond to {cvm.Email} with " +
+                             $"your response to the following message: <br/>{cvm.Message}";
+
+            MailMessage mm = new MailMessage("admin@dani-dev.com", "Wilhelm_Danielle@Outlook.com", cvm.Subject, message);
+
+            mm.IsBodyHtml = true;
+            mm.Priority = MailPriority.High;
+            mm.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.dani-dev.com");
+            client.Credentials = new NetworkCredential("admin@dani-dev.com", "horror11121!");
+
+            try
+            {
+                client.Send(mm);
+            }
+            catch (Exception ex)
+            {
+                ViewBag.CustomerMessage = $"We're Sorry, your request could not be completed at this time." +
+                                          $" Please try again later. Error Message: <br/> {ex.StackTrace}";
+                return View(cvm);
+            }
+
+
+            return View("EmailConfirmation", cvm);
+        }
+
+
     }
 }
